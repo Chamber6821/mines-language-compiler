c0313e510fd4055e1f4e692594b9c7322a2862ce
Chamber6821 <vania.osinin@yandex.ru>
2021-06-13 13:08:59 +0300

    test: update index.ts

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import Lexer  from './Lexer';
+import Parser from './Parser';
+import util from 'util'
+
+const code = `
+void loop {
+    const A = 5
+    const B = (A + 10) * (6 + 5)
+}
+`;
+
+// const compiler = new Compiler(
+//     new Parser(
+//         new Lexer(code)
+//     )
+// )
+
+const lexer = new Lexer(code);
+const parser = new Parser(lexer)
+
+
+try {
+    const rootNode = parser.buildTree()
+    console.log(util.inspect(rootNode, {
+        colors: true,
+        depth: 100
+    }))
+} catch (e) {
+    console.log(e)//.toString())
+    process.exit(1)
+}
+
+process.exit(0)
